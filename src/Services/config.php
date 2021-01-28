21112df3a700cadd45339190a982eb8170810a63
shu <39612999+shugineign@users.noreply.github.com>
2021-01-28 15:03:10 +0900

    up

--- src/Services/config.php
+++ src/Services/config.php
@@ -2,4 +2,8 @@
 return array(
     // if value is true, use production api
     'production' => env('GMO_API_PRODUCTION', false),
+
+    // if use 3D secure
+    'secure_redirect_html' => env('GMO_SECURE_RIDIRECT_HTML', '/your/path/to/RidirectPage'),
+    'secure_tran_url' => env('GMO_SECURE_TRAN_URL', 'http://localhost/clientSample'),
 );
